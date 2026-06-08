26466211fe466e10774e33764ff63d5e20192c02
Muhammad Arifin Ilham Siregar <ilhamsiregar3286@gmail.com>
2026-06-08 18:06:07 +0700

    inilah py

---
+++ python.py
@@ -0,0 +1,12 @@
+print("belajar python")
+tt = True
+tr = False
+
+if (tt == True):
+    if (tr == True):
+        print("trie")
+    else:
+        print("tree")
+
+if (tt == True and tr == True): 
+    print(123)
